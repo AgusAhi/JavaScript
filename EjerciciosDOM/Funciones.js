baee4d9c1b43cb8dd9c675b19791e2415e4c2ba9
AgusAhi <aluahi5617@ieselcaminas.org>
2024-03-22 17:09:21 +0100

    commit

--- EjerciciosDOM/Funciones.js
+++ EjerciciosDOM/Funciones.js
@@ -8,8 +8,35 @@ function cambiarColor() {
     h1.style.color = '#cb3234';
 }
 
-// por hacer
 function añadirTexto() {
     let div = document.getElementById('contenedor');
-    div.innerHTML = "<p>este es el texto de la div acabada de crear</p>";
+    div.innerHTML = "<p>Este es el texto de la div acabada de crear</p>";
 }
+
+function eliminarDiv() {
+    let div = document.getElementById('contenedor');
+    div.remove();
+}
+
+function ponerPie(fig){
+	var pie = fig.getElementsByTagName('figcaption')[0];
+	var img = fig.getElementsByTagName('img')[0];
+
+	if (pie.innerText ==''){
+   	    pie.innerText = img.alt;
+   	}
+	else{
+   	    pie.innerText = '';
+ 	}
+}
+
+function fuentesImg(){
+    var listaImgs = document.getElementsByTagName('img');
+    var salida = document.getElementById('fuentes');
+    var ind;
+    for(ind = 0; ind <listaImgs.length; ind++){
+        salida.innerHTML += listaImgs[ind].src+"<br>";
+       }
+    }
+  
+
