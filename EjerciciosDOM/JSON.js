112669aa34dbab12b18a914f9dd7e42b9e9cbcb1
AgusAhi <aluahi5617@ieselcaminas.org>
2024-04-16 19:02:55 +0200

    update

--- EjerciciosDOM/JSON.js
+++ EjerciciosDOM/JSON.js
@@ -1,14 +1,26 @@
-let text = '{"employees":[' +
+let text1 = '{"employees":[' +
 '{"firstName":"John","lastName":"Doe" },' +
 '{"firstName":"Anna","lastName":"Smith" },' +
 '{"firstName":"Peter","lastName":"Jones" }]}';
 
-const obj = JSON.parse(text);
+const obj = JSON.parse(text1);
 
 for (let i = 0; i < obj.employees.length; i++) {
     document.getElementById("demo").innerHTML +=
     obj.employees[i].firstName + " " + 
     obj.employees[i].lastName + "<br>";
 }
+
+
+
+
+
+
+
+
+   
+
+
+
 
 
